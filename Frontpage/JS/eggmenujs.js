fddeb350ce114dff784c8776978e2fc83f20096a
Erik Maresia <maresiaerik@gmail.com>
2018-03-21 13:12:13 +0200

    All new files with p5, updated direcctory tree.

---
+++ Frontpage/JS/eggmenujs.js
@@ -0,0 +1,24 @@
+
+
+//SIDEBAR SCRIPT
+$(document).ready(function(){
+  $(".fa-times").click(function(){
+    $(".sidebar_menu").addClass("hide_menu");
+    $(".toggle_menu").addClass("opacity_one");
+  });
+
+  $(".toggle_menu").click(function(){
+    $(".sidebar_menu").removeClass("hide_menu");
+    $(".toggle_menu").removeClass("opacity_one");
+  });
+});
+
+//LOG IN FORM SCRIPT
+// GET THE MODEL
+var modal = document.getElementById('form01');
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
