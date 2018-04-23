5da032f35d40c3e4041b3aea4880dd34f82231d0
Erik Maresia <maresiaerik@gmail.com>
2018-04-23 14:14:03 +0300

    Edited loading screen

--- Frontpage/JS/loadingScreen.js
+++ Frontpage/JS/loadingScreen.js
@@ -1,53 +1,52 @@
 
 
-let startbutton = $('#startbutton');
-
+// $(document).ready( () =>
+// {
+//
+// });
 
 function StartGame()
 {
-
   $('#canvas').fadeIn(100, 'swing');
-
   $('#loadingScreen').fadeOut(100, 'swing');
-
 }
 
 
-$(document).ready(function()
+function StartLoading()
 {
+
+  $('#loadingScreen').fadeIn(900, 'swing');
+  let startbutton = $('#startbutton');
   let egg = document.getElementById('eggimg');
+  let loadingScreenH1 = document.getElementById('h1');
+
+  setTimeout( () => { $('#h1').css('visibility', 'visible').hide().fadeIn(2000, 'swing'), 50});
 
   egg.src = '../images/yoshiegg.png';
   egg.style.marginLeft = '200px';
 
-  let h1interval = setInterval(function ()
+  let dotDotDotInterval = setInterval( () =>
   {
-    let h1 = document.getElementById('h1');
-    h1.innerHTML += '.';
-    if (h1.innerHTML.length > 22)
+    loadingScreenH1.innerHTML += '.';
+    if (loadingScreenH1.innerHTML.length > 22)
     {
-      console.log('test');
-      h1.innerHTML = 'Egg Hunt is loading'
+      loadingScreenH1.innerHTML = 'Egg Hunt is loading'
     }
   }, 390);
 
-  setTimeout(function ()
+  setTimeout( () =>
   {
     $('#startbutton').fadeIn(400, 'swing');
-    clearInterval(h1interval);
-    h1.innerHTML = 'Start Playing!'
-    h1.style.marginLeft = 35 + '%';
+    clearInterval(dotDotDotInterval);
+    loadingScreenH1.innerHTML = 'Start Playing!';
+    loadingScreenH1.style.marginLeft = 38.7 + '%';
   }, 5000);
 
   let margin_left = 150;
 
-  let timer = setInterval(function ()
-  {
-    if (margin_left < -5)
-    {
-      margin_left = 150;
-    }
-      margin_left -= 10;
-      egg.style.marginLeft = margin_left + 'px';
+  let timer = setInterval( () => {
+
+    ( margin_left < -5 ) ? (margin_left = 150) : (margin_left -= 10), (egg.style.marginLeft = margin_left + 'px');
+
   }, 100);
-});
+}
