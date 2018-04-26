343ecc82b06a9f7977b41a9c2dfeb57e32249814
Erik Maresia <maresiaerik@gmail.com>
2018-04-26 14:18:36 +0300

    Loading Screen bugfixes

--- Frontpage/JS/loadingScreen.js
+++ Frontpage/JS/loadingScreen.js
@@ -29,17 +29,17 @@ function StartLoading()
   let eggLoadingAnimation = setInterval( () => {( margin_left < bunny_position ) ? (margin_left = 150) : (margin_left -= 10), (egg.style.marginLeft = margin_left + 'px'); }, 100);
 
   //The start button fades in and heading changes text and margin
-  // is changed when loading is done
-  setTimeout( () =>
-  {
-    $('#startbutton').fadeIn(600, 'swing');
-    clearInterval(dotDotDotInterval);
-    loadingScreenH1.innerHTML = 'Start Playing!';
-    loadingScreenH1.style.textAlign = 'center';
-    loadingScreenH1.style.margin = 'auto';
-    $('#startbutton').css('display', 'flex');
+// is changed when loading is done
+setTimeout( () =>
+{
+  $('#startbutton').fadeIn(600, 'swing');
+  clearInterval(dotDotDotInterval);
+  loadingScreenH1.innerHTML = 'Start Playing!';
+  loadingScreenH1.style.textAlign = 'center';
+  loadingScreenH1.style.margin = 'auto';
+  $('#startbutton').css('display', 'flex');
 
-  }, 1000);
+}, 1000);
 }
 
 // Start button is pressed after 'loading' is done,
@@ -47,16 +47,16 @@ function StartLoading()
 
 function StartGame()
 {
-  $('#loadingScreen').fadeOut(100, 'swing');
-  $('#myCanvas').fadeIn(500, 'swing');
-  $('.bunny').css('animation-play-state', 'paused');
-  clearInterval(eggLoadingAnimation);
-  $('#eggimg').css('visibility', 'hidden');
-  $('.bunnyStatic').css('display', 'inline-block');
+$('#loadingScreen').fadeOut(400, 'swing', () =>  $('#myCanvas').fadeIn(200, 'swing') );
+$('.bunny').css('animation-play-state', 'paused');
+clearInterval(eggLoadingAnimation);
+$('#eggimg').css('visibility', 'hidden');
+$('.bunnyStatic').css('display', 'inline-block');
 // <<<<<<< HEAD
-  // $('#myCanvas').css('visibility', 'visible').css('display', 'none').fadeIn(200, 'swing');
+// $('#myCanvas').css('visibility', 'visible').css('display', 'none').fadeIn(200, 'swing');
 // =======
-  // document.getElementById('myCanvas').style.display = 'inline';
+// document.getElementById('myCanvas').style.display = 'inline';
 // // >>>>>>> d80ea0fcc4c2247af6667c5f73a3fc01f202f1c4
+
 
 }
